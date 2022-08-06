10a74d17c773d03c787c77a743b158a093acf380
Zahil Chuduang <zahil.c@ku.th>
2022-08-06 14:00:24 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(PostSeeder::class);
+        $this->call(TagSeeder::class);
+        $this->call(CommentSeeder::class);
+
+        $tags = Tag::all();
+
+        Post::all()->each(function ($post) use ($tags) {
+            $post->tags()->attach(
+                // a post will have 1-5 tags randomly
+                $tags->random(rand(1, 5))->pluck('id')
+            );
+        });
+
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
